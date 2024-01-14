faf34a3594954d7d2d8a0cf3e00c52e2b4e57228
Jomkit <j.jujaroen96@gmail.com>
2024-01-14 19:58:11 +0700

    implemented becrypt passwords

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 from models import db, connect_db, User
 from forms import RegisterForm, LoginForm
 from SECRETS import secret_key
@@ -26,9 +26,10 @@ def register():
     if form.validate_on_submit():
         data = form.data
         data.pop('csrf_token')
-        new_user = User(**data)
+        new_user = User.register(**data)
         db.session.add(new_user)
         db.session.commit()
+        session['username'] = new_user.username
         
         flash(f'Account registered, welcome {new_user.username}!', 'success')
         return redirect('/secret')
@@ -43,10 +44,10 @@ def login():
         username = form.username.data
         password = form.password.data
 
-        user = User.query.filter_by(username=username).first()
+        user = User.authenticate(username, password)
 
-        if user and (password == user.password):
-
+        if user:
+            session['username'] = user.username #keep logged in
             flash(f'Welcome back {user.username}', 'info')
             return redirect('/secret')
         
@@ -55,8 +56,21 @@ def login():
     
     return render_template('login-form.html', form=form)
 
+@app.route('/logout', methods=['POST'])
+def logout():
+    """Logs out user and redirects to login"""
+    session.pop("username")
+    flash('Come back soon!', 'success')
+    return redirect('/login')
+
 @app.route('/secret')
 def show_secret():
-    users = User.query.all()
+    if session.get('username'):
+        username = session['username']
+        user = User.query.filter_by(username=username).first()
+        users = User.query.all()
 
-    return render_template('secret.html', users=users)
+        return render_template('secret.html', users=users, user=user)
+    else:
+        flash('Please login to access secret page', 'danger')
+        return redirect('/login')
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def connect_db(app):
     """Connect to database"""
@@ -17,3 +19,30 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name):
+        """
+        Register a user and hash their password for security
+        Accept all user info and create new user
+        """
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode('utf8')
+
+        # Return instance of user w/ hashed pwd
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+    
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate existence of user and password is correct
+
+        Return user if valid, else return False
+        """
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            # Return user instance
+            return user
+        else:
+            return False
