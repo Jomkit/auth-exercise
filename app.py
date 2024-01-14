a0e7ba66c5b5c340a504dee104b03232eebae9c0
Jomkit <j.jujaroen96@gmail.com>
2024-01-15 05:43:58 +0700

    currently implementing feedback functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, flash, session
-from models import db, connect_db, User
+from models import db, connect_db, User, Feedback
 from forms import RegisterForm, LoginForm
 from SECRETS import secret_key
 
@@ -21,6 +21,11 @@ def redirect_to_register():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """Handle registering new users"""
+
+    if session.get('username'):
+        flash('Already logged in', 'warning')
+        return redirect(f"/users/{session['username']}")
+    
     form = RegisterForm()
 
     if form.validate_on_submit():
@@ -32,12 +37,18 @@ def register():
         session['username'] = new_user.username
         
         flash(f'Account registered, welcome {new_user.username}!', 'success')
-        return redirect('/secret')
+        return redirect(f'/users/{new_user.username}')
 
     return render_template('register.html', form=form)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    """Get login page and redirect to user page if user log in info correct or
+    if logged in user attempts to access login page"""
+    if session.get('username'):
+        flash('Already logged in', 'warning')
+        return redirect(f"/users/{session['username']}")
+    
     form = LoginForm()
 
     if form.validate_on_submit():
@@ -49,12 +60,37 @@ def login():
         if user:
             session['username'] = user.username #keep logged in
             flash(f'Welcome back {user.username}', 'info')
-            return redirect('/secret')
+            return redirect(f'/users/{user.username}')
         
         flash('User credentials incorrect', 'warning')
         return redirect('/login')
     
     return render_template('login-form.html', form=form)
+
+@app.route('/users/<string:username>')
+def show_user_info(username):
+    """Show user's information except password"""
+
+    # No user logged in
+    if session.get('username', False) == False:
+        flash('Please login', 'danger')
+        return redirect('/login')
+
+    # User logged in, fetch user data for user page attempting to be access
+    user = User.query.filter_by(username=username).first()
+    # Check session user aka logged in user is same as user being accessed
+    if session.get('username', False) == user.username:
+        # send to user page
+        return render_template('user-info.html', user=user)
+    else: 
+        # send to own user page
+        flash('Attempted to access another user page', 'warning')
+        return redirect(f'/users/{session["username"]}')
+    
+@app.route('/users/<string:username>/feedback/add', methods=['GET', 'POST'])
+def feedback_form(username):
+    
+    return render_template('feedback-form.html')
 
 @app.route('/logout', methods=['POST'])
 def logout():
